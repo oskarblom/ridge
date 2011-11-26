ad73dcac4e447ac8fa0ace46b3fb597ec094dd2b
Mattias Hising <hising@gmail.com>
2011-11-26 11:31:49 +0100

    Added required libs and new scope for app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-$(function(){
-	window.Area = Backbone.Model.extend({});
-})
+/*
+    app.js requires:
+    
+    - jQuery
+    - Underscore
+    - Backbone
+    - JSON2
+    - Mustache?
+*/
+
+(function($){
+    $(function(){
+    	area = Backbone.Model.extend({});
+    })
+})(jQuery);
+
+
+
