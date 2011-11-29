d581cc2a375a13eb7be4781829de431c6868dbdb
Mattias Hising <hising@gmail.com>
2011-11-29 19:01:20 +0100

    added more struct

--- js/app.js
+++ js/app.js
@@ -20,25 +20,63 @@
     	    }
     	});
     	
+    	App.bind('added', function(ad){
+            console.log(ad);
+    	})
+    	
     	/* A Collection */
     	AdList = Backbone.Collection.extend({
-    	    model : App,
-    	    localStorage : new Store('ads')
+    	    model : App
+    	    // localStorage : new Store('ads')
     	});
     	
     	ads = new AdList;
     	
     	/* The View */
     	AdView = Backbone.View.extend({
-    	    tagName : "li",
-    	    template : $('#ad-template').html(),
-    	    events : {
-    	        "click .reply" : "reply"
+    	    initialize : function(){
+    	        this.render();
     	    },
-    	    reply : function(){
-    	        //Reply
+    	    render : function(){
+    	        var template = _.template($('#ad_template').html(), {});
+    	        this.el.html(template);
+    	    },
+    	    events: {
+    	        "click input[type=submit]" : "addAd",
+    	        "change #ad_rent_type" : 'massageUI'
+    	    },
+    	    addAd : function(event){
+    	        alert("add ad");
+    	        return false;
+    	    },
+    	    massageUI : function(event){
+    	        console.log(event);
     	    }
     	});
+    	
+    	var ad_view = new AdView({el : $('#ad_container')})
+    	
+    	var AppController = Backbone.Controller.extend({
+    	    routes : {
+    	        '' : 'index',
+    	        '/ad' : 'list_ads',
+    	        '/ad/:id' : 'show_ad'
+    	    },
+    	    index : function(){
+    	        alert("show home page");
+    	    },
+    	    list_ads : function(){
+    	        alert("show ads page");
+    	    },
+    	    show_ad : function(ad_id){
+    	        alert("show ad with id:" + ad_id);
+    	    }
+    	    
+    	})
+    	
+    	// var ApplicationController = new AppController;   
+    	// 
+        Backbone.history.start({pushState: true});
     	
     })
 })(jQuery);
